7a5a22031069e87e5761951af8141543f0946f34
hitomi-takebe <takebe.hitomi@gmail.com>
2024-11-05 00:59:51 +0900

    入力情報の分数化

--- JS/jquery.js
+++ JS/jquery.js
@@ -1,4 +1,4 @@
-console.log("開始");
+console.log("現在時刻を表示");
 // 現在時刻を表示する関数
 function updateTime() {
     const date = new Date();
@@ -7,34 +7,39 @@ function updateTime() {
     const seconds = date.getSeconds();
 
     // HTMLのid="time"の要素に現在時刻を表示
-    console.log("timeの要素に現在時刻を表示");
     $("#time").text(date.toLocaleTimeString());
     // 時間データをオブジェクトとして返す
-    console.log("時間データをオブジェクトとして返す");
     return { hour: hours, minute: minutes, second: seconds };
 }
 
-// アラーム機能
-function alerm() {
-    const currentTime = updateTime();
-    console.log(`現在の時刻は${currentTime.hour}時${currentTime.minute}分${currentTime.second}秒になります。`);
-}
-    // アラームを設定したい時間
 
+// 入力情報の取得と表示
 $("#button1").click(function () {
+    // テキストボックスのvalue値を取得
     const time2 = $("#mtg_time").val();
     const hours2 = $("#pre_hours").val();
-    const minutes2 = $("#pre_mins").val();      // テキストボックスのvalue値を取得
+    const minutes2 = $("#pre_mins").val();  
+    // spanタグに値を設定
     $("#span1").text(time2);
     $("#span2").text(hours2);
-    $("#span3").text(minutes2);      // spanタグに値を設定
-    });
+    $("#span3").text(minutes2);      
 
-//     if (currentTime.hour === alerm_hour && currentTime.minute === alerm_minute) {
-//         $("#alerm_text").text("時間になりました。");
-//         console.log(`現在の時刻が${alerm_hour}時${alerm_minute}分になりました。`);
-//         setTimeout(endAlerm, 30000); // 30秒後にアラームメッセージを消す
-//     }
+    console.log("準備・移動の時間をpreに取得");
+    // diffに準備・移動時間の分数を入力
+    let diff = hours2 * (60 * 60 * 1000) + minutes2 * (60 * 1000);
+    console.log(Math.abs(pre) / (60 * 1000),"diffに準備・移動に必要な分数を記録しています。");
+    console.log(time2.getTime())
+
+    // let pre = time2.getTime() - diff.getTime();
+    // console.log(`準備に必要な時間は${pre().hour}時${pre().minute}分になります。`)
+    // console.log(`現在の時刻は${date().hour}時${date().minute}分${date().minute}秒になります。`)
+    });
+    
+
+// if (currentTime.hour === alerm_hour && currentTime.minute === alerm_minute) {
+//     $("#alerm_text").text("時間になりました。");
+//     console.log(`現在の時刻が${alerm_hour}時${alerm_minute}分になりました。`);
+//     setTimeout(endAlerm, 30000); // 30秒後にアラームメッセージを消す
 // }
 
 // アラームメッセージを非表示にする
@@ -82,6 +87,6 @@ function endSale() {
 }
 
 // 1秒ごとに各機能を実行
-setInterval(updateTime, 1000); // 現在時刻の更新
-setInterval(alerm, 1000);      // アラームのチェック
-setInterval(sale, 1000);       // セールのチェック
+// setInterval(updateTime, 1000); // 現在時刻の更新
+// setInterval(alerm, 1000);      // アラームのチェック
+// setInterval(sale, 1000);       // セールのチェック
